905227376c321ff79a8ffe55c1efdbce5cd76263
Amol Rama <ramaa@rose-hulman.edu>
2024-03-19 11:46:53 -0400

    feat(website): initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        shantell: ["Shantell Sans", "sans serif"],
+        foundation: ["Edu NSW ACT Foundation", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
